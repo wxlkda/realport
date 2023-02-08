8888941ff979c7eab00f6fa98457f627d66e5a98
wxlkda <674445@pdsb.net>
2023-02-07 22:46:44 -0500

    created the experience socials and projects div templates

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import './css/Intro.css';
 import './css/About.css';
 import './css/Navigation.css';
+import './css/Socials.css';
+import './css/Experience.css';
+import './css/Projects.css';
 
 import React from 'react';
-//import Sidebar from "./otherjs/Sidebar"
 //import HomeIcon from '@mui/icons-material/Home';
 //TODO: Add anchor links after the thing is odne. In order to do this, we gotta add IDS
 
@@ -39,7 +41,7 @@ function About() {
   return (
     <div id="about">
       <span className="title">
-      /about-me
+        <b>/about-me</b>
       </span>
       <div className="about-info body-text">
         I am currently a <b>Computer Science</b> student studying at the <span className="special"><b> Toronto Metropolitan University</b></span> (formerly known as <span className="special">Ryerson</span>).
@@ -52,12 +54,64 @@ function About() {
   );
 };
 
+function Experience() {
+  return (
+    <div id="experience">
+      <span className="title">
+        <b>/experience</b>
+      </span>
+      <div className="experience-info body-text">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it to make a type
+      specimen book. It has survived not only five centuries, but also the leap into
+      electronic typesetting, remaining essentially unchanged. It was popularised 
+      in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+      and more recently with desktop publishing software like Aldus PageMaker including
+      versions of Lorem Ipsum.
+      </div>
+    </div>
+  );
+};
+
+function Projects() {
+  return (
+    <div id="projects">
+      <span className="title">
+        <b>/software-creations</b>
+      </span>
+      <div className="project-info body-text">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it to make a type
+      specimen book. It has survived not only five centuries, but also the leap into
+      electronic typesetting, remaining essentially unchanged. It was popularised 
+      in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+      and more recently with desktop publishing software like Aldus PageMaker including
+      versions of Lorem Ipsum.
+      </div>
+    </div>
+  );
+};
+
+
+
 function Socials() {
   return (
     <div id="socials">
       <span className="title">
-        socials
+      <b>/socials</b>
       </span>
+      <div className="social-info body-text">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it to make a type
+      specimen book. It has survived not only five centuries, but also the leap into
+      electronic typesetting, remaining essentially unchanged. It was popularised 
+      in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+      and more recently with desktop publishing software like Aldus PageMaker including
+      versions of Lorem Ipsum.
+      </div>
     </div>
   );
 };
@@ -75,19 +129,18 @@ class Navigation extends React.Component {
       ];
 
 
-    this.state = {selectedColor : "#ccd6f6", notSelectedColor : "#8892b0"}; 
 
     this.styles = {
       selected: {
         textAlign: "center",
         marginLeft: "3%",
-        color: this.state.selectedColor,
+        color: "#ccd6f6",
         textDecoration: "none"
       },
       default: {
         textAlign: "center",  
         marginLeft: "3%",
-        color: this.state.notSelectedColor,
+        color: "#8892b0",
         textDecoration: "none"
       }
     };
@@ -96,18 +149,19 @@ class Navigation extends React.Component {
     
     this.content = <a></a>;
   }
-  changeColor = () => {
-    this.setState({selectedColor : "#ccd6f6", notSelectedColor : "#8892b0"});
+  changeColor = (event) => {
+    event.target.style.color = "#ccd6f6";
+    
   }
-  resetColor = () => {
-    this.setState({selectedColor : "#8892b0", notSelectedColor : "#ccd6f6"});
+  resetColor = (event) => {
+    event.target.style.color = "#8892b0";
   }
   returnJSX = (tab) => {
     if (tab.id === this.selected) {
-      this.content = <a style = {this.styles.selected} href={tab.link}>{tab.name}</a>;
+      this.content = <a style = {this.styles.selected} href={tab.link} >{tab.name}</a>;
     }
     else {
-      this.content = <a style = {this.styles.default} href={tab.link} onMouseOver={this.changeColor} onMouseLeave={this.resetColor}>{tab.name}</a>;
+      this.content = <a style = {this.styles.default} href={tab.link} onMouseOver= {event => this.changeColor(event)} onMouseLeave={event => this.resetColor(event)}>{tab.name}</a>;
     }
     return this.content;
   }
@@ -133,6 +187,9 @@ export default function App() {
       <div id="content">
         <Intro></Intro>
         <About></About>
+        <Experience></Experience>
+        <Projects></Projects>
+        <Socials></Socials>
         <div className="footer">
         <center className="footer-text">
           Made and designed by Alwalid Khan.
