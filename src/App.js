bf8f7676fbe6b01c3a3beb58163c7ee1289ba9e4
wxlkda <674445@pdsb.net>
2023-02-13 01:12:19 -0500

    RAHHH

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import React from 'react';
 //import HomeIcon from '@mui/icons-material/Home';
 //TODO: Add anchor links after the thing is odne. In order to do this, we gotta add IDS
 
+const me = require('./images/me.png');
 
 
 
@@ -44,11 +45,23 @@ function About() {
         <b>/about-me</b>
       </span>
       <div className="about-info body-text">
+        <img src={me} className="about-image" height="320" width="320"></img>
         I am currently a <b>Computer Science</b> student studying at the <span className="special"><b> Toronto Metropolitan University</b></span> (formerly known as <span className="special">Ryerson</span>).
-        Outside of school, I enjoy board games such as <b>chess</b> and <b>catan</b>. Frequently, I also   play online video games with my friends.
+        I plan to get a graduate with a Bachelors and aspire to become a <b>Software Engineer</b> at a high-level company.
         <br></br>
         <br></br>
-        Below are some of the technologies that I am familiar with:
+        Below are some of the <b>technologies</b> that I am familiar with:
+        <ul className="skills">
+          <li>Javascript</li>
+          <li>HTML & CSS</li>
+          <li>Python</li>
+          <li>AWS</li>
+          <li>React</li>
+          <li>Java</li>
+          <li>MongoDB</li>
+          <li>Git</li>
+        </ul>
+        At home, I enjoy playing board games such as <span className="special">chess</span> and <span className="special">catan</span>. I sometimes also play online video games with my friends.
       </div>
     </div>
   );
@@ -191,14 +204,14 @@ export default function App() {
         <Projects></Projects>
         <Socials></Socials>
         <div className="footer">
-        <center className="footer-text">
-          Made and designed by Alwalid Khan.
-        </center>
-        <br></br>
-        <center className="footer-text">
-          All rights reserved ©.
-        </center>
-      </div>
+          <center className="footer-text">
+            Made and designed by Alwalid Khan.
+          </center>
+          <br></br>
+          <center className="footer-text">
+            All rights reserved ©.
+          </center>
+        </div>
       </div>  
     </div>
   );
