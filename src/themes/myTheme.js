111f06dbe3451a5bede85d1909c4069edb8e8314
wxlkda <674445@pdsb.net>
2023-03-27 04:18:52 -0400

    smth jus snapped

---
+++ src/themes/myTheme.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from 'material-ui/styles';
+
+export default createMuiTheme({
+    palette: {
+        primary: { // works
+          main: '#165788',
+        }
+    },
+});
