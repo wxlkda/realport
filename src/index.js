c8533f678ae803fefee5aa667c7914777bae3679
wxlkda <674445@pdsb.net>
2023-03-27 04:32:03 -0400

    updated site title

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import NavProvider from './context/NavContext'
 
 const base = ReactDOM.createRoot(document.getElementById('base'));
 
-var title = "portfolio";
+var title = "Alwalid";
 document.title = title;
 
 
