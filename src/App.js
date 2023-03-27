ab13d72d138dfc10b1164fbcb9384a307e7c66f8
wxlkda <674445@pdsb.net>
2023-03-27 04:23:35 -0400

    Swapped projs and exp.

--- src/App.js
+++ src/App.js
@@ -91,6 +91,14 @@ function Experience() {
       in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
       and more recently with desktop publishing software like Aldus PageMaker including
       versions of Lorem Ipsum.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it to make a type
+      specimen book. It has survived not only five centuries, but also the leap into
+      electronic typesetting, remaining essentially unchanged. It was popularised 
+      in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+      and more recently with desktop publishing software like Aldus PageMaker including
+      versions of Lorem Ipsum.
       </div>
     </div>
   );
@@ -164,8 +172,8 @@ export default function App() {
         <div id="content">
           <Intro></Intro>
           <About></About>
-          <Experience></Experience>
           <Projects></Projects>
+          <Experience></Experience>
           <div className="footer">
             <center className="footer-text">
               Made and designed by Alwalid Khan.
