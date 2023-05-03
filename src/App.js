ce60d28674546ceaa15d4a7ae09ab95aa789da3f
wxlkda <674445@pdsb.net>
2023-05-02 22:47:22 -0400

    Fixed elements looking weird when adding fade in animation; switched from span to div and added inline-block display

--- src/App.js
+++ src/App.js
@@ -50,22 +50,26 @@ function Intro() {
             .
           </span>
         </div>
+
+        <FadeInSection>
           <div className="intro-rt">
             Making something innovative.
           </div>
-        
-        <span className="intro-inspirational">
-        Some inspiring text here that will inspire here single person that reads this message. Some inspiring text here that will inspire here single person that reads this message.   
-        </span>
-        
-        <br></br>
-        <a className="messageBox" href="mailto:alwalid23dec@gmail.com?subject=Hello!" rel="noopener noreferrer">
-          <span className="messageBoxText">
           
-          <svg class="MuiSvgIcon-root sayEmail" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-.4 4.25l-7.07 4.42c-.32.2-.74.2-1.06 0L4.4 8.25c-.25-.16-.4-.43-.4-.72 0-.67.73-1.07 1.3-.72L12 11l6.7-4.19c.57-.35 1.3.05 1.3.72 0 .29-.15.56-.4.72z"></path></svg>
-          Say hi!
-          </span>
-        </a>
+          <div className="intro-inspirational">
+          Some inspiring text here that will inspire here single person that reads this message. Some inspiring text here that will inspire here single person that reads this message.   
+          </div>
+          <br></br>
+
+          <a className="messageBox" href="mailto:alwalid23dec@gmail.com?subject=Hello!" rel="noopener noreferrer">
+            <div className="messageBoxText">
+            
+              <svg class="MuiSvgIcon-root sayEmail" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-.4 4.25l-7.07 4.42c-.32.2-.74.2-1.06 0L4.4 8.25c-.25-.16-.4-.43-.4-.72 0-.67.73-1.07 1.3-.72L12 11l6.7-4.19c.57-.35 1.3.05 1.3.72 0 .29-.15.56-.4.72z"></path></svg>
+              Say hi!
+            </div>
+
+          </a>
+        </FadeInSection>
       </div>
   );
 };
@@ -217,9 +221,7 @@ export default function App() {
   return (
     <div className="App">
         <div id="content">
-        <FadeInSection>
-          <Intro></Intro>
-        </FadeInSection>
+        <Intro></Intro>
         <FadeInSection>
           <About></About>
         </FadeInSection>
