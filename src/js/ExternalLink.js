ee08fe0129cc32c567223e426294b2024a9dd900
wxlkda <674445@pdsb.net>
2023-05-19 16:51:37 -0400

    Added 1 more project, fixed box size for 3rd proj

--- src/js/ExternalLink.js
+++ src/js/ExternalLink.js
@@ -5,14 +5,16 @@ import OpenInBrowserIcon from '@mui/icons-material/OpenInBrowser';
 function ExternalLinks(props) {
   return (
     <span className="external-links">
-      <a className="github-icon" href={props.githubLink}>
-        <GitHubIcon
-          style={{
-            fontSize: 20,
-            color: "var(--cloudy-white)"
-          }}
-        ></GitHubIcon>
+      {props.githubLink && (
+        <a className="github-icon" href={props.githubLink}>
+          <GitHubIcon
+            style={{
+              fontSize: 20,
+              color: "var(--cloudy-white)"
+            }}
+          ></GitHubIcon>
       </a>
+      )}
       {props.openLink && (
         <a className="open-icon" href={props.openLink}>
           <OpenInBrowserIcon
--- src/js/Projects.js
+++ src/js/Projects.js
@@ -15,23 +15,30 @@ function Projects() {
     const projectsList = {
         "Data Visualizer": {
           desc:
-            "A website used to visualize numerous algorithms on arrays, graphs, and trees. Created to highlight my knowledge of data structures and algorithms.",
+          "A website used to visualize numerous algorithms on arrays, graphs, and binary trees. Created to highlight my knowledge of data structures and algorithms.",
           techStack: "HTML / CSS, Javascript",
           link: "https://github.com/wxlkda/datavis",
           open: "https://data.alwalidkhan.com/"
         },
         "Takumi A.I.": {
           desc:
-          "A 2D runner game made in PyGame, where the goal is to survive the longest against obstacles. Features a NEAT (NeuroEvolution of Augmenting Topologies) algorithm",
+          "A 2D runner game made in PyGame, where the goal is to survive the longest against obstacles. Features a Neuroevolution of Augmenting Topologies algorithm.",
           techStack: "Python",
           link: "https://github.com/wxlkda/TakumiAI",
           open: ""
         },
-        "Athena": {
+        "Verve": {
           desc:
-          "A placeholder for one of my future projects. I am still working on this project rn.",
-          techStack: "HTML & CSS. Javascript, C++, Python, Keras, TensorFlow.",
-          link: "https://google.ca",
+          "A dynamic social networking app, created to enable sharing of personalized content and fostered for interactive experiences. Allows users to build conversations and share their experiences.",
+          techStack: "Express.js, MongoDB, Node.js, React.js",
+          link: "",
+          open: ""
+        },
+        "Equinox": {
+          desc:
+          "A discord bot made during the COVID-19 pandemic using Python. Serves as a moderation bot, with the intention to help keep servers secure from bots/raiders hosted on a Heroku Server",
+          techStack: "Python, SQLite, SQL, Pillow",
+          link: "",
           open: ""
         }
       };
@@ -50,13 +57,15 @@ function Projects() {
                                 <FolderOpenRoundedIcon style={{ fontSize: 35 }}></FolderOpenRoundedIcon>
                             </div>
                             <span className="external-links">
-                                <a className="github-icon" href={projectsList[key]["link"]} rel="noopener noreferrer" target="_blank">
+                                {projectsList[key]["link"] && (
+                                  <a className="github-icon" href={projectsList[key]["link"]} rel="noopener noreferrer" target="_blank">
                                     <GitHubIcon style = {{fontSize: 23}}></GitHubIcon>
-                                </a>
+                                  </a>
+                                )}
                                 {projectsList[key]["open"] && (
                                   <a className="open-icon" href={projectsList[key]["open"]} rel="noopener noreferrer" target="_blank">
-                                  <OpenInBrowserIcon style = {{fontSize: 25}}></OpenInBrowserIcon>
-                              </a>
+                                    <OpenInBrowserIcon style = {{fontSize: 25}}></OpenInBrowserIcon>
+                                  </a>
                                 )}
                             </span>
                         </div>
